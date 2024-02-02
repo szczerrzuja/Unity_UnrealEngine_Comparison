9bcdcf1f399bef99d2a80d2304e80377e1d90e55
Kacper Szczerrzuja Rudź <57622414+szczerrzuja@users.noreply.github.com>
2024-02-02 17:54:31 +0100

    UnrealEngineStart

---
+++ UnrealPart/Source/UnrealPart/UnrealPart.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPart.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPart, "UnrealPart" );
---
+++ UnrealPart/Source/UnrealPart/UnrealPartGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealPartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPART_API AUnrealPartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
